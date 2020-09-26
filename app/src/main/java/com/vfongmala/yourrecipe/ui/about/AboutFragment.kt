2ea033f6760eb3ff203e89a8549ae7c68381e406
vfongmala <vichita.fongmala@agoda.com>
2020-09-26 10:21:15 +0700

    Implement about page

--- app/src/main/java/com/vfongmala/yourrecipe/ui/about/AboutFragment.kt
+++ app/src/main/java/com/vfongmala/yourrecipe/ui/about/AboutFragment.kt
@@ -1,5 +1,7 @@
 package com.vfongmala.yourrecipe.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,23 +11,37 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.vfongmala.yourrecipe.R
+import com.vfongmala.yourrecipe.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
 
-    private lateinit var aboutViewModel: AboutViewModel
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        aboutViewModel =
-            ViewModelProvider(this).get(AboutViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_about, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        aboutViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        initView()
+        return binding.root
+    }
+
+    private fun initView() {
+        binding.aboutGithub.setOnClickListener {
+            openUrl(R.string.github_url)
+        }
+        binding.aboutLinkedin.setOnClickListener {
+            openUrl(R.string.linkedin_url)
+        }
+    }
+
+    private fun openUrl(urlResId: Int) {
+        val url = getString(urlResId)
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        }
+        startActivity(intent)
     }
 }
