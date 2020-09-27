388ae2ab12cb8d39c042e2498f9ab8848bcdb60a
vfongmala <vichita.fongmala@agoda.com>
2020-09-27 21:31:57 +0700

    Ignore if save existing recipe

--- app/src/main/java/com/vfongmala/yourrecipe/dao/RecipeDao.kt
+++ app/src/main/java/com/vfongmala/yourrecipe/dao/RecipeDao.kt
@@ -3,6 +3,7 @@ package com.vfongmala.yourrecipe.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.vfongmala.yourrecipe.dao.entity.SavedRecipe
 
@@ -12,6 +13,6 @@ interface RecipeDao {
     @Query("SELECT * FROM SAVED_RECIPE")
     fun loadAll(): LiveData<List<SavedRecipe>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun save(data: SavedRecipe)
 }
--- app/src/main/java/com/vfongmala/yourrecipe/dao/SavedRecipeRepository.kt
+++ app/src/main/java/com/vfongmala/yourrecipe/dao/SavedRecipeRepository.kt
@@ -6,7 +6,6 @@ import com.vfongmala.yourrecipe.dao.entity.SavedRecipe
 class SavedRecipeRepository(
     private val savedRecipeDao: RecipeDao
 ) {
-
     val savedRecipes: LiveData<List<SavedRecipe>> = savedRecipeDao.loadAll()
 
     fun save(savedRecipe: SavedRecipe) {
